b138fd582553226edb6bcf5428c3ff3fd4942936
MehmetcCan <75257589+MehmetcCan@users.noreply.github.com>
2021-02-10 00:08:02 +0300

    Delete events directory

--- events/guildMemberAdd.js
+++
@@ -1,5 +0,0 @@
-module.exports = member => {
-    let username = member.user.username;
-    member.sendMessage('Selamlar,Değerli **' + username + '** Discord Sunucumaza Hoşgeldin :kissing_heart:  :thinking: Keyifli Vakit Geçirmen Dileğiyle. Buda sunucumuzun sınırsız linki. :wink:');
-
-};
